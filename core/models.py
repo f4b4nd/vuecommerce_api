6a2e9b88738fd2791792669c6d9a980d4c66748e
Fabien <fabien.andrian@gmail.com>
2020-12-17 19:37:59 +0100

    pipenv

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    title = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    resume = models.TextField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    img = models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.pk} - {self.title}"
--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vuecommerce project.
+WSGI config for ecommerce project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
