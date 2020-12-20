52be1c893210b2a2a22431bb487b5a52b6190a0b
Fabien <fabien.andrian@gmail.com>
2020-12-20 13:16:14 +0100

    minor

--- accounts/api.py
+++ accounts/api.py
@@ -1,11 +1,10 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import UserSerializer, RegisterSerializer
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 
 # Register Api
-
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
@@ -18,3 +17,15 @@ class RegisterAPI(generics.GenericAPIView):
             "token": AuthToken.objects.create(user)
         })
     
+# LoginAPI
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)
+        })
