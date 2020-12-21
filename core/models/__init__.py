d9334a23c2414621242e0835311fa0f23c65f184
Fabien <fabien.andrian@gmail.com>
2020-12-21 19:25:51 +0100

    minor

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,22 +1,2 @@
-from django.db import models, IntegrityError, transaction
-from django.utils.text import slugify
-
 from .product import *
-
-
-class TemplateHTML(models.Model):
-    title = models.CharField(max_length=100, blank=True, null=True)
-    body = models.TextField(blank=True, null=True)
-    slug = models.SlugField(max_length=250, unique=True, blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.id} - {self.title}"
-
-    def save(self, *args, **kwargs):
-        try:
-            with transaction.atomic():
-                self.slug = slugify(self.title)
-                super().save(*args, **kwargs)
-        except IntegrityError:
-            self.slug = f"{slugify(self.title)}-{self.id}"
-            super().save(*args, **kwargs)
+from .template import *
