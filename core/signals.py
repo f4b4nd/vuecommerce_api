cebd5144e2fc20283abe7d35f5227dc31daacd6e
Fabien <fabien.andrian@gmail.com>
2020-12-22 15:27:05 +0100

    media with local storage

---
+++ core/signals.py
@@ -0,0 +1,27 @@
+from django.dispatch import receiver
+from django.db.models import signals
+from django.conf import settings
+from rest_framework.authtoken.models import Token
+import os
+
+from .models import Product
+
+
+@receiver(signals.pre_save, sender=Product)
+def on_image_delete(sender, instance, **kwargs):
+    # Removes img file from media when Product.img field is removed
+
+    if not instance.pk:
+        return False
+    
+    old_file = Product.objects.get(pk=instance.pk).img
+    
+    if not old_file:
+        return False
+
+    new_file = instance.img
+
+    if  old_file != new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
+    
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -26,7 +28,9 @@ urlpatterns = [
 ]
 
 
-from django.conf import settings
+# ONLY WORKS WHEN settings.DEBUG, for production: use NGINX
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 # DEBUG
 if settings.DEBUG:
