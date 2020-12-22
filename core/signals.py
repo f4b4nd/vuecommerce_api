93b980621349e245a782f71c24d071da1f295590
Fabien <fabien.andrian@gmail.com>
2020-12-22 18:01:10 +0100

    minor

--- core/signals.py
+++ core/signals.py
@@ -17,14 +17,14 @@ def on_product_img_delete(sender, instance, **kwargs):
     if not instance.pk:
         return False
     
-    old_file = Product.objects.get(pk=instance.pk).img
+    old_img_file = Product.objects.get(pk=instance.pk).img
 
-    if not old_file:
+    if not old_img_file:
         return False
 
-    new_file = instance.img
+    new_img_file = instance.img
 
-    if  old_file != new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
+    if  old_img_file != new_img_file:
+        if os.path.isfile(old_img_file.path):
+            os.remove(old_img_file.path)
     
